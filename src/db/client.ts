267ff73affe42299ac65dfe569f5dffaf59e8989
MuhammadAman02 <mohammada@hst.ie>
2025-07-16 11:40:42 +0500

    update file

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,7 +1,7 @@
-// src/db/client.ts
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import * as schema from "./schema";
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from '@neondatabase/serverless';
+import { env } from '../config/env';
+import * as schema from './schema';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
